1472d5339bd034b754c783b82807a3eaa12eb11c
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-09-21 10:41:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const h3 = document.getElementById("letterCount");
 let num = 1;
 input.addEventListener("keydown",(event)=>{
 	const key = event.key;
-	h3.innerText = num;
 	if(key === "Backspace" && num !== 0){
 		h3.innerText = num;
 		num--;
 	}
 	else{
 		
+		h3.innerText = num;
 		num++;
 	}
 })
